9cc16fc06b87e9173a99e6c6c0e57a998dc72409
Sam847-tech <65718947+Sam847-tech@users.noreply.github.com>
2020-05-28 21:28:18 +0530

    Create program5.py

---
+++ day5/program5.py
@@ -0,0 +1,13 @@
+lst = [5,2,55,11,56,23,10,19,4,7,9,28,16]
+odd = []
+even =[]
+for i in lst:
+    if i%2 == 0:
+        even.append(i)
+    else:
+        odd.append(i)
+        
+odd.sort()
+even.sort()
+lst = odd[::-1] + even
+print('The array having odd no sorted in descending order and even no in ascending order is  :  \n\n{}'.format(lst))
