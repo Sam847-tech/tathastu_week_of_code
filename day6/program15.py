6ec35ed06cd44f6540177d884fe0572b67157d34
Sam847-tech <65718947+Sam847-tech@users.noreply.github.com>
2020-05-28 21:37:06 +0530

    Create program15.py

---
+++ day6/program15.py
@@ -0,0 +1,23 @@
+n = int(input("Enter size of array: "))
+arr = []
+
+for i in range(n):
+    arr.append(int(input("Enter value"))) 
+   
+    
+for i in range(1, n-1):
+    flag = 0
+    for j in range(i):
+        if arr[j] > arr[i]:
+            flag = 1
+            break
+    for j in range(i+1, n):
+        if arr[j] < arr[i]:
+            flag = 1
+            break
+
+    if flag==0:
+        print(arr[i], "is the answer")
+        exit()
+        
+print("No such number found")
