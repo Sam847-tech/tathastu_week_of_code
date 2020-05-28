769e075511864dba4dd4e0044476ab3ca70e3e4b
Sam847-tech <65718947+Sam847-tech@users.noreply.github.com>
2020-05-28 21:25:59 +0530

    Create program2.py

---
+++ day5/program2.py
@@ -0,0 +1,17 @@
+size = int(input('Enter the number of total values  :  '))
+
+
+lst = []
+for i in range(size):
+    lst.append(int(input('Please enter the value  :  ')))
+    
+    
+
+def rep(lst):
+    
+    for i in range(size-1):
+        lst[i] = max(lst[i+1:])
+    return lst
+
+    
+print('The list obtained after replacing all values with maximun value in its right side  :  {}'.format(rep(lst)))
