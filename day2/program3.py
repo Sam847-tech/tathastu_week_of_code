4497236d21c3f543f0465fde7a68f1a18b72771e
Sam847-tech <65718947+Sam847-tech@users.noreply.github.com>
2020-05-28 21:15:59 +0530

    Create program3.py

---
+++ day2/program3.py
@@ -0,0 +1,9 @@
+n = int(input("Enter the value of N: "))
+
+print("\n")
+
+for i in range(n):
+    print(" " * i + "*" + "  " * (n - 1 - i) + "*")
+
+for i in range(n):
+    print(" " * (n - 1 - i) + "*" + "  " * i + "*")
